fad7411d13da0e4e6ed7898da80f425669b7998f
Streamweaver <Streamweaver@gmail.com>
2014-04-29 14:58:43 -0400

    Added more infor to s3 listing output.

--- s3_list_buckets.py
+++ s3_list_buckets.py
@@ -13,7 +13,7 @@ from collections import defaultdict
 def convertSize(num_bytes, unit='gb'):
 	"Converts bytes to the unit scale provided."
 	unit = unit.lower()
-	if unit == 'b': return "%d B" % num_bytes
+	if unit == 'b' or num_bytes < 1024: return "%d B" % num_bytes
 	units = {
 		'kb': 1, 'mb': 2, 'gb': 3, 'tb': 4, 'pb': 5, 'eb': 6,  
 	}
@@ -24,12 +24,27 @@ def convertSize(num_bytes, unit='gb'):
 	return "%f %s" % (f_bytes, unit.upper())
 
 if __name__ == "__main__":
-	b = defaultdict(int)
+	b = defaultdict(lambda: defaultdict(int))
+	storage = {
+		'count': 0,
+		'total': 0,
+		'largest': 0,
+	}
 	s3 = S3Connection()
 
 	for bucket in s3.get_all_buckets():
 		for key in bucket.list():
-			b[bucket.name] += key.size
+			b[bucket.name]['size'] += key.size
+			b[bucket.name]['count'] += 1
+			storage['count'] += 1
+			storage['total'] += key.size
+			if key.size > b[bucket.name]['largest']:
+				b[bucket.name]['largest'] = key.size
+			if key.size > storage['largest']:
+				storage['largest'] = key.size
 
-	for bucket, size in b.iteritems():
-		print "%s  >  %s" % (bucket, convertSize(size))
+	for bucket, stats in b.iteritems():
+		print("%s  >  %s files totalling %s; largest: %s" % (bucket, stats['count'], convertSize(stats['size']), convertSize(stats['largest'])))
+
+	print("-" * 79)
+	print("Overall S3 Stats - %s files totalling %s; largest: %s" % (storage['count'], convertSize(storage['total']), convertSize(storage['largest'])))
